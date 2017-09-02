fa81cc5a80702efc2fd41c94f70b07b185e11d83
bowdar <bowdar@163.com>
2017-09-02 22:05:05 +0800

    Update and rename BPSample.cpp to BPNNSample.cpp

--- BPNNSample.cpp
+++ BPNNSample.cpp
@@ -1,12 +1,12 @@
-﻿#include "BPNeuralNetwork.hpp"
+﻿#include "BPNN.hpp"
 
 int main()
 {
     /// 1. 创建一个输入层，两个隐含层，一个输出层的神经网络
-    mtl::BPNeuralNet<20, 30, 20, 2> mynn;
+    mtl::BPNN<20, 30, 20, 2> bpnn;
 	
     /// 2. 初始化
-    mynn.init(0.1, 0.8);
+    bpnn.init(0.1, 0.8);
 
     /// 3. 输入
     mtl::Matrix<double, 1, 20> inMatrix;
@@ -14,8 +14,9 @@ int main()
     ///    录入你的矩阵数据...
 	
     /// 4. 训练
-    mynn.train(inMatrix, outMatrix, 100);
+    bpnn.train(inMatrix, outMatrix, 100);
 	
     /// 5. 仿真
-    mynn.simulate(inMatrix, outMatrix);
+    bpnn.simulate(inMatrix, outMatrix);
 }
+
